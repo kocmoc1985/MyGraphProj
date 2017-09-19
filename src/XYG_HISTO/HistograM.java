0037879f3185485773d97bd436ae11f35acdfabb
KOCMOC <KOCMOC@192.168.2.12>
2017-09-19 15:27:29 +0200

    ...

--- src/XYG_HISTO/HistograM.java
+++ src/XYG_HISTO/HistograM.java
@@ -4,15 +4,11 @@
  */
 package XYG_HISTO;
 
+import XYG_BASIC.DiffMarkerAction;
 import XYG_BASIC.HelpA;
-import XYG_BASIC.MyGraphContainer;
+import XYG_BASIC.MyGraphXY;
 import XYG_BASIC.MyPoint;
-import XYG_BASIC.MySerie;
 import XYG_BASIC.MyXYGB;
-import XYG_BASIC.PointHighLighter;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -21,21 +17,28 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JFrame;
 
 /**
  *
  * @author KOCMOC
  */
-public class HistograM extends MyXYGB {
+public class HistograM extends MyXYGB implements DiffMarkerAction {
 
     public TreeMap<Double, Integer> histoMap = new TreeMap();
     public ArrayList<String> xValuesList;
+    public ResultSet resultSet;
+    public String valueColName;
+    public String round;
 
-    public HistograM(String title, MyGraphXY_H h, int displayMode) {
-        super(title, h, displayMode);
+    public HistograM(String title, MyGraphXY_H xY_H, int displayMode) {
+        super(title, xY_H, displayMode);
     }
 
+    @Override
+    public void markersSet(MyGraphXY trigerInstance,MyPoint markerA, MyPoint markerB) {
+        rebuildData(resultSet, valueColName, round, markerA.getPointIndex(), markerB.getPointIndex());
+    }
+    
     public void reset() {
         deleteAllPointsFromAllSeries();
         histoMap = new TreeMap<Double, Integer>();
@@ -50,20 +53,44 @@ public class HistograM extends MyXYGB {
         h.setStepIdentifierX(x);
     }
 
-    public void addData(ResultSet rs, String valueColName, String round) {
+    /**
+     * Good to keep as example though
+     * @deprecated 
+     * @param rs 
+     */
+    public void addLimits(ResultSet rs) {
         try {
-            while (rs.next()) {
-                double val = rs.getDouble(valueColName);
-                buildHistogramDataSet(val, histoMap, round);
-            }
-
-            addPoints();
-
+            double minLim = rs.getDouble("LSL");
+            double maxLim = rs.getDouble("USL");
+            setLimits(minLim, maxLim);
         } catch (SQLException ex) {
             Logger.getLogger(HistograM.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
+    public void addData(ResultSet rs, String valueColName, String round) {
+        //
+        this.resultSet = rs;
+        this.valueColName = valueColName;
+        this.round = round;
+        //
+        reset();
+        refresh();
+        //
+        try {
+            while (rs.next()) {
+                double val = rs.getDouble(valueColName);
+                buildHistogramDataSet(val, histoMap, round);
+            }
+            //
+            addPoints();
+            //
+        } catch (SQLException ex) {
+            Logger.getLogger(HistograM.class.getName()).log(Level.SEVERE, null, ex);
+            addPoints();
+        }
+    }
+    
     public void addData(double[] values, String round) {
         for (double val : values) {
             buildHistogramDataSet(val, histoMap, round);
@@ -78,6 +105,45 @@ public class HistograM extends MyXYGB {
         }
         addPoints();
     }
+
+    public void rebuildData(ResultSet rs, String valueColName, String round, int start, int end) {
+        //
+        getSerie().deleteAllPoints();
+        histoMap.clear();
+        //
+        int x = 0;
+        try {
+            //
+            rs.first();
+            //
+            while (rs.next()) {
+                if (x >= start && x <= end) {
+                    double val = rs.getDouble(valueColName);
+                    buildHistogramDataSet(val, histoMap, round);
+                }
+                x++;
+            }
+            //
+            addPoints();
+            //
+        } catch (SQLException ex) {
+            Logger.getLogger(HistograM.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void rebuildData(ArrayList<MyPoint> points,String round) {
+        //
+        getSerie().deleteAllPoints();
+        histoMap.clear();
+        //
+        for (MyPoint myPoint : points) {
+             buildHistogramDataSet(myPoint.y_Display, histoMap, round);
+        }
+        //
+        addPoints();
+    }
+    
+    
 
     public void addPoints() {
         //
@@ -122,81 +188,5 @@ public class HistograM extends MyXYGB {
     private void updateMap(Object key, Object value, TreeMap map) {
         map.remove(key);
         map.put(key, value);
-    }
-
-    @Override
-    public void initializeA() {
-        this.setTitleSize(20, true);
-        this.setTitleSize(20, true);
-        this.setTitleColor(Color.black);
-//        this.setBorderHeadAndFootComponents(BorderFactory.createLineBorder(Color.darkGray));
-        this.setHeadHeight(0.1);
-        //
-        // setAxisScaling(...) & setDrawGrid(...) influence each other!
-        this.setAxisScaling(true, true);
-//        this.setDrawGrid(true);
-        this.setShowPopUpLeftClick(true);
-        this.setPointHighLighterEnabled(true);
-        this.setDisableScalingWhenGrid();
-        this.setGridColor(Color.black);
-//        this.setScaleXYaxisLength(1.9);
-        //
-//        this.setBackgroundColorOfGraph(Color.BLACK);
-        this.setDrawMarker(true);
-        this.setMarkerDotted(true);
-        this.setMarkerInfo(1);
-        this.setMarkerAutoReset(false);
-    }
-
-    @Override
-    public void initializeB() {
-        serie = new MySerie(getTitle());
-        //
-        serie.setDrawPoints(true);
-        serie.setPointThickness(1);
-//        serie.setPointHighLightColor(Color.red);
-//        serie.setPointColor(Color.red);
-
-        serie.setDrawLines(true);
-        serie.setLineThickness(1);
-        serie.setLineDotted();
-        serie.setCurveColor(Color.red);
-        serie.setOverallScale(true);
-        //
-        this.addSerie(serie);
-        //
-        PointHighLighter.addSerie(serie);
-    }
-
-    public static void main(String[] args) {
-        //
-        HistograM hm = new HistograM("speed", new MyGraphXY_H(), MyGraphContainer.DISPLAY_MODE_FULL_SCREEN);
-        //
-        JFrame jf = new JFrame(hm.getTitle());
-        jf.setSize(new Dimension(800, 800));
-        jf.setLayout(new GridLayout(1, 0));
-        jf.add(hm.getGraph());
-        jf.setVisible(true);
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-        double[] dataSet = {1.5, 1.5, 1.5, 1.5, 3.2, 3.2, 4.3, 4.3, 4.5, 5.5, 5.5, 5.5, 5.2, 5.2, 5.2, 7.8, 7.8, 8.3, 8.3, 8.6, 8.9, 9.2, 9.2, 9.2};
-
-//        msxyg.addDataSetBySerie(dataSet, "speed");
-
-        hm.addData(dataSet, "#.#");
-
-//        hm.addData(arr(200, 10, 15));
-
-        //
-//        HelpA.addMouseListenerToAllComponentsOfComponent(jf.getRootPane());
-    }
-
-    public static int[] arr(int n, double min, double max) {
-        int[] array = new int[n];
-        for (int i = 0; i < n; i++) {
-            array[i] = (int) (Math.random() * max + min);
-        }
-        return array;
     }
 }
