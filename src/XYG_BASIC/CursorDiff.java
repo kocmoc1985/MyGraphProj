e32a91674d54514104e2e965922e2584a26b0f14
KOCMOC <KOCMOC@192.168.2.12>
2017-09-21 15:50:08 +0200

    ...

--- src/XYG_BASIC/CursorDiff.java
+++ src/XYG_BASIC/CursorDiff.java
@@ -12,7 +12,6 @@ import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
-import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 
 /**
