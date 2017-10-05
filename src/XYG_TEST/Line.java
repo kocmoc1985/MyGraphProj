814013334de6c6e45f70b212acc0b03007b58896
KOCMOC <KOCMOC@192.168.2.12>
2017-10-05 12:11:54 +0200

    ...

--- src/XYG_TEST/Line.java
+++ src/XYG_TEST/Line.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package unused;
+package XYG_TEST;
 
 
 import java.awt.BasicStroke;
--- src/XYG_TEST/MyGraph_T.java
+++ src/XYG_TEST/MyGraph_T.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package XYG_T;
+package XYG_TEST;
 
 import java.awt.Color;
 import java.awt.Graphics;
